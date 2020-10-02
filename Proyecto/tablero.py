658126124a1261b80abc08c9c33d18c81aa48d68
Camilo Cuello <ccuello@unal.edu.co>
2020-10-02 16:52:09 -0500

    Se sube el tablero con el problema de los cuadros random solucionados.

--- Proyecto/tablero.py
+++ Proyecto/tablero.py
@@ -12,42 +12,45 @@ class Squares(pygame.sprite.Sprite):
         """
         self.square_size = [90,90]
         self.square_pos = [x_pos,y_pos]
-        self.color = randint(1,3)
+        self.color = randint(1,6)
     
     def Trivia_UP(self):
         """
         Casillas que contienen preguntas y adelantan al jugador.
         """
         pygame.draw.rect(screen, BLUE, [self.square_pos,self.square_size])
+        pygame.draw.rect(screen, BLACK, [self.square_pos, self.square_size], 1)
     
     def Trivia_DOWN(self):
         """
         Casillas que contienen preguntas que al ser incorrectas devuelven al jugador.
         """
         pygame.draw.rect(screen, ORANGE, [self.square_pos,self.square_size])
+        pygame.draw.rect(screen, BLACK, [self.square_pos, self.square_size], 1)
 
     def Trivia_NONE(self):
         """
         Casillas que no generan ninguna acción, es decir que son estáticas.
         """
         pygame.draw.rect(screen, GREEN, [self.square_pos,self.square_size])
+        pygame.draw.rect(screen, BLACK, [self.square_pos, self.square_size], 1)
 
     def DrawSquare(self):
         """
         Método que activa un color de casilla aleatoriamente.
         """
-        if self.color == 1:
+        if self.color == 1 or self.color == 4:
             self.Trivia_UP()
-        elif self.color == 2:
+        elif self.color == 2 or self.color == 5:
             self.Trivia_DOWN()
-        elif self.color == 3:
+        elif self.color == 3 or self.color == 6:
             self.Trivia_NONE()
 
 # Definir Colores
 BLACK  = [0,0,0]
 WHITE  = [255,255,255]
 RED    = [255,0,0]
-BLUE   = [0,0,255]
+BLUE   = [31,151,255,100]
 GREEN  = [0,255,0]
 PURPLE = [127,96,252,92]
 ORANGE = [255,136,22,100]
@@ -56,31 +59,31 @@ pygame.init() # Inicializar ventana
 screen_size = [900, 540]
 screen = pygame.display.set_mode(screen_size) # Medidas
 pygame.display.set_caption("Tablero")
-clock = pygame.time.Clock()
-done = False
-colors = []
+clock = pygame.time.Clock() # Controla las fps
+running = True
 
-#for i in range(0,61):
-#    rand_color = randint(1,3)
-#    colors.append(rand_color)
-# 
-#print(colors)
+fill_controller = 0
 
-squares_list = pygame.sprite.Group()
-all_sprites_list = pygame.sprite.Group()
-
-
-while not done: # Bucle infinito para mantener ventana abierta.
+while running: # Bucle infinito para mantener ventana abierta.
     for event in pygame.event.get(): # Bucle que recibe eventos.
         if event.type == pygame.QUIT:# Condicional para cerrar la ventana al presionar la (x).
-            done = True
-    # Rellenar fondo
-    screen.fill(WHITE)
+            running = False
+    # ---Rellenar fondo
+    #    NONE
 
-    # Dibujar
-    for i in range(0, 900, 90):
-        for j in range(0, 540, 90):
-            square = Squares(i, j).DrawSquare()
+    # ---Lógica
 
-    pygame.display.flip()
-    clock.tick(60)
+
+
+    # ----******----
+
+    # ----Dibujar
+    while fill_controller < 1: # Ciclo para que dibuje los cuadrados solo una vez.
+        screen.fill(WHITE)
+        for i in range(0, 900, 90):
+            for j in range(0, 540, 90):# Ciclo for clásico para dibujar una matriz.
+                square = Squares(i, j).DrawSquare()
+        fill_controller += 1
+
+    pygame.display.flip()# Refresca la ventana
+    clock.tick(60) # 60fps
