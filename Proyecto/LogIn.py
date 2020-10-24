c0e079d9ca2663ce0532fa9f4ee75921c6780f9c
SantiagoOsp <71992747+SantiagoOsp@users.noreply.github.com>
2020-10-24 14:31:32 -0500

    Add files via upload

--- Proyecto/LogIn.py
+++ Proyecto/LogIn.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 from tkinter import filedialog
 import sqlite3
 import tablero
+
 class Aplicacion:
     def __init__(self):
         # ----------CARACTERISTECAS DE VENTANA-------------------
@@ -12,11 +13,11 @@ class Aplicacion:
         self.sh = self.root.winfo_screenheight()
         self.x = self.sw // 3
         self.y = self.sh // 4
-        self.root.title("LogIn")
+        self.root.title("MindQuare")
         self.root.iconbitmap("Resources\Images\GameLogo.ico")
-        self.root.geometry(f"450x430+{self.x}+{self.y}")#root.geometry(anchoxalto+padx+pady)
+        self.root.geometry(f"720x405+{self.x}+{self.y}")#root.geometry(anchoxalto+padx+pady)
         self.root.resizable(width=False, height=False)
-        self.root.config(bg="#F0F1F2")
+        self.root.config(bg="white")
 
         # -----------IMAGEN INTERFAZ------------------------
 
@@ -24,65 +25,64 @@ class Aplicacion:
         self.imagenLogo = Label(
             self.root,
             image=self.imagenPrincipal,
-            width=200,
-            height=200,
+            width=720,
+            height=405,
             justify="center",
         )
-        self.imagenLogo.config(bg="#F0F1F2")
+        self.imagenLogo.config(bg="white")
         self.imagenLogo.pack()
 
         # ------------------FRASE BIENVENIDA--------------------
-
+        '''
         self.Bienvenida = Label(
             self.root,
             text="Bienvenido al Sistema de ingreso de este juego\n Ingrese sesión",
-            bg="#F0F1F2",
+            bg="purple",
             fg="black",
             font=("Times New Roman", 17),
         )
-        self.Bienvenida.place(x=2, y=205)
-
+        self.Bienvenida.place(x=150, y=205)
+        '''
         # ------------------LOG IN-----------------------------
         self.emailLabel = Label(
             self.root,
             text="Introduzca su E-mail:",
-            bg="#F0F1F2",
             fg="black",
             font=("Times New Roman", 10),
         )
-        self.emailLabel.place(x=35, y=300)
+        self.emailLabel.place(x=195, y=250)
         self.emailvar = StringVar()
         self.emailEntry = Entry(self.root, textvariable=self.emailvar, width=40)
-        self.emailEntry.place(x=150, y=300)
+        self.emailEntry.place(x=315, y=250)
         self.emailEntry.focus()
 
         self.passLabel = Label(
             self.root,
             text="Introduzca su password:",
-            bg="white",
+            bg="#C0C0C0",
             fg="black",
             font=("Times New Roman", 10),
         )
-        self.passLabel.place(x=15, y=340)
+        self.passLabel.place(x=175, y=290)
         self.passvar = StringVar()
         self.passlEntry = Entry(
             self.root, textvariable=self.passvar, show="*", width=40
         )
-        self.passlEntry.place(x=150, y=340)
-
-        # ------------------BOTÓN LOG IN--------------------
-
-        self.bLogIn = Button(self.root, text="SIGN UP", background='blue',command=self.signUp)
-        self.bLogIn.place(x=60, y=380)
+        self.passlEntry.place(x=315, y=290)
 
         # ------------------BOTÓN SING UP--------------------
 
-        self.bSign = Button(self.root, text="LOG IN", command=self.logIn)
-        self.bSign.place(x=300, y=380)
+        self.bLogIn = Button(self.root, text="SIGN UP", background='#FFE052',command=self.signUp)
+        self.bLogIn.place(x=240, y=325)
+
+        # ------------------BOTÓN LOG IN---------------------
+
+        self.bSign = Button(self.root, text="LOG IN", background= '#FFE052' ,command=self.logIn)
+        self.bSign.place(x=420, y=325)
 
         # -------------------BOTÓN DE SALIR------------------------
-        self.bSalir = Button(self.root, text="salir", command=self.root.destroy)
-        self.bSalir.pack(side=BOTTOM)
+        self.bSalir = Button(self.root, text="salir", background= '#FFE052' ,command=self.root.destroy)
+        self.bSalir.place(x=340,y=360)
 
         self.conexion_db()
 
@@ -132,7 +132,7 @@ class ventanaRegistro:
         self.registro.geometry(f"450x430+{self.x}+{self.y}")
         self.registro.resizable(width=False, height=False)
         self.registro.config(bg="#F0F1F2")
-
+        
         # ---------------TÍTULO---------------------------
         self.primerFrame = Frame(self.registro)
         self.primerFrame.pack()
@@ -151,7 +151,7 @@ class ventanaRegistro:
         self.nombreEntry = Entry(self.segundoFrame, textvariable=self.nombreVar, width=40)
         self.nombreEntry.grid(row=0, column=1, pady=18, padx=10)
         self.nombreEntry.focus()
-
+        '''
         self.apellidoLabel = Label(
             self.segundoFrame, text="Apellido:", font=("Times New Roman", 12)
         )
@@ -160,13 +160,13 @@ class ventanaRegistro:
         self.apellidoVar = StringVar()
         self.apellidoEntry = Entry(self.segundoFrame, textvariable=self.apellidoVar, width=40)
         self.apellidoEntry.grid(row=1, column=1, pady=18, padx=10)
+        '''
+        self.NICKLabel = Label(self.segundoFrame, text="Nickname:", font=("Times New Roman", 12))
+        self.NICKLabel.grid(row=2, column=0, pady=18, padx=10)
 
-        self.DNILabel = Label(self.segundoFrame, text="DNI:", font=("Times New Roman", 12))
-        self.DNILabel.grid(row=2, column=0, pady=18, padx=10)
-
-        self.DNIVar = StringVar()
-        self.DNIEntry = Entry(self.segundoFrame, textvariable=self.DNIVar, width=40)
-        self.DNIEntry.grid(row=2, column=1, pady=18, padx=10)
+        self.NICKVar = StringVar()
+        self.NICKEntry = Entry(self.segundoFrame, textvariable=self.NICKVar, width=40)
+        self.NICKEntry.grid(row=2, column=1, pady=18, padx=10)
 
         self.emailLabel = Label(self.segundoFrame, text="E-mail:", font=("Times New Roman", 12))
         self.emailLabel.grid(row=3, column=0, pady=18, padx=10)
@@ -214,7 +214,7 @@ class ventanaRegistro:
     def registrar(self):
         colectaNombre = str(self.nombreVar.get())
         colectaApellido = str(self.apellidoVar.get())
-        colectaDNI = str(self.DNIVar.get())
+        colectaNICK = str(self.NICKVar.get())
         colectaEmail = str(self.emailVar.get())
         colectaPass = str(self.passVar.get())
         arrobas = colectaEmail.count("@")
@@ -224,7 +224,7 @@ class ventanaRegistro:
         if (
             colectaNombre != ""
             or colectaApellido != ""
-            or colectaDNI != ""
+            or colectaNICK != ""
             or colectaEmail != ""
             or colectaPass != ""
         ):
@@ -253,7 +253,7 @@ class ventanaRegistro:
         if validadorE and validadorP:
             self.miCursor.execute(
                 "INSERT INTO USUARIOS VALUES('"
-                + colectaDNI
+                + colectaNIKC
                 + "','"
                 + colectaNombre
                 + "','"
@@ -265,7 +265,7 @@ class ventanaRegistro:
                 + "')"
             )
             self.miCursor.execute(
-                "SELECT DNI FROM USUARIOS WHERE DNI=" + colectaDNI
+                "SELECT NICK FROM USUARIOS WHERE NICK=" + colectaDNI
             )
             self.datoId = self.miCursor.fetchall()
             self.miConexion.commit()
