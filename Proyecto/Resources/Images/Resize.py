ca6c4527109bc4823f4c84b8481f0333609f4585
Camilo <ccuello@unal.edu.co>
2020-10-27 12:03:09 -0500

    'Update'

--- Proyecto/Resources/Images/Resize.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-image = Image.open('GameLogo.png')
-new_image = image.resize((550, 405))
-new_image.save('GameLogoR.png')
-
-print(image.size) # Output: (1200, 776)
-print(new_image.size) # Output: (400, 400)
